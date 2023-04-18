7049de4283815d896acc12e3fc846247a50c2579
tangtianlong <tianlong.ttl@antgroup.com>
2023-04-18 10:49:33 +0800

    first

---
+++ src/test/java/net/tiaotiaolong/maven_test/MavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tiaotiaolong.maven_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
